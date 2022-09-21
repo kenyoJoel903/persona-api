d2746e7dac4a4f64a9019bee1363e2f4970497b8
Kenyo Joel Pecho Ñaupari <externo5.avantica@confianza.pe>
2022-09-21 07:52:06 -0500

    configuracion de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const functionLambda = "PersonaApi";
+  const config = new DocumentBuilder()
+  .setTitle(functionLambda)
+  .setDescription(`${functionLambda} test`)
+  .setVersion('1.0')
+  .addTag(functionLambda)
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc/swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
